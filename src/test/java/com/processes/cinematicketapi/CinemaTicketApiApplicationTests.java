905aba47143db45c8a10a01e87e0ef023c220f6d
Skipper <janswierkowski@interia.pl>
2023-11-10 10:49:33 +0100

    Git innit

---
+++ src/test/java/com/processes/cinematicketapi/CinemaTicketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.processes.cinematicketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
