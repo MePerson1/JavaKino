acfd7ded2b315d14e4609a5db5171efea1623b9e
Skippi-engineer <83069081+Skippi-engineer@users.noreply.github.com>
2023-11-11 15:52:38 +0100

    Added Services and NotFoundException

---
+++ src/main/java/com/processes/cinematicketapi/service/MovieService.java
@@ -0,0 +1,41 @@
+package com.processes.cinematicketapi.service;
+
+import com.processes.cinematicketapi.exeptions.NotFoundException;
+import com.processes.cinematicketapi.models.Movie;
+import com.processes.cinematicketapi.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService
+{
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    // Metody:
+    public Movie getMovieById(Long id)
+    {
+        return movieRepository.findById(id).orElseThrow(() -> new NotFoundException("Movie not found with id: " + id));
+    }
+
+    public Movie getMovieByTitle(String title)
+    {
+        return movieRepository.findByTitle(title);
+    }
+
+    public List<Movie> getAllMovies()
+    {
+        return movieRepository.findAll();
+    }
+
+    public Movie save(Movie movie)
+    {
+        return movieRepository.save(movie);
+    }
+}
---
+++ src/main/java/com/processes/cinematicketapi/service/ScreeningService.java
@@ -0,0 +1,37 @@
+package com.processes.cinematicketapi.service;
+
+import com.processes.cinematicketapi.exeptions.NotFoundException;
+import com.processes.cinematicketapi.models.Screening;
+import com.processes.cinematicketapi.repository.ScreeningRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ScreeningService
+{
+    private final ScreeningRepository screeningRepository;
+
+    @Autowired
+    public ScreeningService(ScreeningRepository screeningRepository)
+    {
+        this.screeningRepository = screeningRepository;
+    }
+
+    //Metody:
+    public Screening getScreeningById(Long id)
+    {
+        return screeningRepository.findById(id).orElseThrow(() -> new NotFoundException("Screening not found with id: " + id));
+    }
+
+    public List<Screening> getScreeningsByMovieTitle(String movieTitle)
+    {
+        return screeningRepository.findByMovieTitle(movieTitle);
+    }
+
+    public Screening save(Screening screening)
+    {
+        return screeningRepository.save(screening);
+    }
+}
